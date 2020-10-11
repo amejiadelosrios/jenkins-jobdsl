fdacef6b6a8049a1307a34cd6f017998f72d9e8b
amejiadelosrios <amejiadelosrios@gmail.com>
2020-10-11 06:46:36 -0500

    adding id

--- jobs/mutibranch.groovy
+++ jobs/mutibranch.groovy
@@ -1,11 +1,11 @@
 multibranchPipelineJob('python-deploy') {
     triggers {
-        periodic(5)
+        periodic(1)
     }
     branchSources {
         git {
             remote("https://github.com/amejiadelosrios/test-jenkins-1.git")
-            credentialsId('amejiadelosrios')
+            id("cbfb0bcb-6c38-4d07-810e-4c84606289e1")
         }
     }
     orphanedItemStrategy {
